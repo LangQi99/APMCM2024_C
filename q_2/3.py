82e296c47f547729d32423054420ad0b78629e6c
LangQi99 <2032771946@qq.com>
2024-11-22 18:42:28 +0800

    upd

--- q_2/3.py
+++ q_2/3.py
@@ -17,6 +17,11 @@ def read_data_file(filename):
     years = [int(year) for year in lines[0].split()[1:]]
     cat_data = [int(num) for num in lines[1].split()[1:]]
     dog_data = [int(num) for num in lines[2].split()[1:]]
+
+    # 反转数据顺序，使年份从小到大排序
+    years.reverse()
+    cat_data.reverse()
+    dog_data.reverse()
     return years, cat_data, dog_data
 
 
@@ -32,61 +37,60 @@ for country, file_path in countries.items():
     years, cat_data, dog_data = read_data_file(file_path)
 
     # 创建ARIMA模型并预测
-    future_years = list(range(years[0] + 1, years[0] + 4))  # 预测未来3年
+    future_years = list(range(years[-1] + 1, years[-1] + 4))  # 使用最后一年作为基准
     all_years = years + future_years
 
     # 对猫的数据进行预测
     cat_model = ARIMA(cat_data, order=(1, 1, 1))
     cat_fit = cat_model.fit()
-    cat_forecast = cat_fit.forecast(steps=3)
-    cat_all_data = np.concatenate([cat_data, cat_forecast])
+    # 修改预测起始点为第一个观测值
+    cat_pred = cat_fit.get_prediction(start=1, end=len(all_years)-1)
+    cat_predicted = np.concatenate(
+        ([cat_data[0]], cat_pred.predicted_mean[:len(years)-1]))
+    cat_forecast = cat_pred.predicted_mean[len(years)-1:]
+    cat_all_data = np.concatenate((cat_predicted, cat_forecast))
 
     # 对狗的数据进行预测
     dog_model = ARIMA(dog_data, order=(1, 1, 1))
     dog_fit = dog_model.fit()
-    dog_forecast = dog_fit.forecast(steps=3)
-    dog_all_data = np.concatenate([dog_data, dog_forecast])
+    # 同样修改狗的预测起始点
+    dog_pred = dog_fit.get_prediction(start=1, end=len(all_years)-1)
+    dog_predicted = np.concatenate(
+        ([dog_data[0]], dog_pred.predicted_mean[:len(years)-1]))
+    dog_forecast = dog_pred.predicted_mean[len(years)-1:]
+    dog_all_data = np.concatenate((dog_predicted, dog_forecast))
 
     # 绘制图表
     plt.figure(figsize=(12, 7))
 
-    # 绘制历史数据（实线）
-    plt.plot(years, cat_data, marker='o',
-             label='Cat', solid_capstyle='round')
-    plt.plot(years, dog_data, marker='o',
-             label='Dog', solid_capstyle='round')
+    # 绘制实际数据（实线）
+    plt.plot(all_years[:len(years)], cat_data, 'o-',
+             label='Cat Actual', solid_capstyle='round')
+    plt.plot(all_years[:len(years)], dog_data, 'o-',
+             label='Dog Actual', solid_capstyle='round')
 
-    # 绘制预测数据（虚线）并连接历史数据和预测数据
-    plt.plot(future_years, cat_forecast, 'o-', label='Predicted Cat')
-    plt.plot([years[0], future_years[0]],
-             [cat_data[0], cat_forecast[0]], 'b--')  # 连接猫的历史和预测数据
+    # 绘制拟合和预测数据（虚线）
+    plt.plot(all_years, cat_all_data, 'o--',
+             label='Cat Fitted & Predicted')
+    plt.plot(all_years, dog_all_data, 'o--',
+             label='Dog Fitted & Predicted')
 
-    plt.plot(future_years, dog_forecast, 'o-', label='Predicted Dog')
-    plt.plot([years[0], future_years[0]], [dog_data[0],
-             dog_forecast[0]], 'C1--')  # 连接狗的历史和预测数据
-
-    # 添加数值标签
-    # for i, txt in enumerate(cat_data):
-    #     plt.annotate(str(txt), (years[i], cat_data[i]), textcoords="offset points", xytext=(
-    #         0, 10), ha='center')
-    # for i, txt in enumerate(dog_data):
-    #     plt.annotate(str(txt), (years[i], dog_data[i]), textcoords="offset points", xytext=(
-    #         0, -15), ha='center')
-
-    # 添加预测值标签
-    for i, txt in enumerate(cat_forecast):
-        plt.annotate(f'{txt:.0f}', (future_years[i], txt), textcoords="offset points", xytext=(
-            0, 10), ha='center')
-    for i, txt in enumerate(dog_forecast):
-        plt.annotate(f'{txt:.0f}', (future_years[i], txt), textcoords="offset points", xytext=(
-            0, -15), ha='center')
+    # 添加所有预测值标签（包括拟合值和预测值）
+    for i, txt in enumerate(cat_all_data):
+        plt.annotate(f'{txt:.0f}', (all_years[i], txt),
+                     textcoords="offset points",
+                     xytext=(0, 10), ha='center')
+    for i, txt in enumerate(dog_all_data):
+        plt.annotate(f'{txt:.0f}', (all_years[i], txt),
+                     textcoords="offset points",
+                     xytext=(0, 10), ha='center')  # 向下偏移以避免重叠
 
     plt.xlabel('Year')
     plt.ylabel('Number')
-    plt.title(f'{country} - Cat and Dog Numbers with Forecast')
+    plt.title(f'{country} - Predicted Cat and Dog Numbers')
     plt.legend()
     plt.grid(True)
 
-    # 保存图表到pri1文件夹
+    # 保存图表
     plt.savefig(f'q_2/pri1/{country.lower()}_pets_forecast.png')
     plt.close()
